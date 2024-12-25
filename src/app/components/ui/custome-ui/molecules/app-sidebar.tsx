499790cc13e7898f0f90de2c2e2f18afd780d194
optiverse.now <optiverse.now@gmail.com>
2024-12-25 18:41:02 +0900

    sidebarLabelの修正

--- src/app/components/ui/custome-ui/molecules/app-sidebar.tsx
+++ src/app/components/ui/custome-ui/molecules/app-sidebar.tsx
@@ -30,7 +30,7 @@ export function AppSidebar() {
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupLabel>Life Platform</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
